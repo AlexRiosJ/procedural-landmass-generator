4944108fdfd500fd3cf3ca7a9a427c7ec2640163
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2021-01-18 01:03:40 -0600

    Adding LOD and fixing seams with normalize modes

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -9,9 +9,11 @@ public class MapGenerator : MonoBehaviour {
     public enum DrawMode { NoiseMap, ColorMap, Mesh }
     public DrawMode drawMode;
 
+    public Noise.NormalizeMode normalizeMode;
+
     public const int mapChunkSize = 241;
     [Range (0, 6)]
-    public int levelOfDetail;
+    public int editorPreviewLOD;
     public float noiseScale;
 
     public int octaves;
@@ -33,42 +35,42 @@ public class MapGenerator : MonoBehaviour {
     Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>> ();
 
     public void DrawMapInEditor () {
-        MapData mapData = GenerateMapData ();
+        MapData mapData = GenerateMapData (Vector2.zero);
         MapDisplay display = FindObjectOfType<MapDisplay> ();
         if (drawMode == DrawMode.NoiseMap) {
             display.DrawTexture (TextureGenerator.TextureFromHeightMap (mapData.heightMap));
         } else if (drawMode == DrawMode.ColorMap) {
             display.DrawTexture (TextureGenerator.TextureFromColorMap (mapData.colorMap, mapChunkSize, mapChunkSize));
         } else if (drawMode == DrawMode.Mesh) {
-            display.DrawMesh (MeshGenerator.GenerateTerrrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColorMap (mapData.colorMap, mapChunkSize, mapChunkSize));
+            display.DrawMesh (MeshGenerator.GenerateTerrrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD), TextureGenerator.TextureFromColorMap (mapData.colorMap, mapChunkSize, mapChunkSize));
         }
     }
 
-    public void RequestMapData (Action<MapData> callback) {
+    public void RequestMapData (Vector2 center, Action<MapData> callback) {
         ThreadStart threadStart = delegate {
-            MapDataThread (callback);
+            MapDataThread (center, callback);
         };
 
         new Thread (threadStart).Start ();
     }
 
-    void MapDataThread (Action<MapData> callback) {
-        MapData mapData = GenerateMapData ();
+    void MapDataThread (Vector2 center, Action<MapData> callback) {
+        MapData mapData = GenerateMapData (center);
         lock (mapDataThreadInfoQueue) {
             mapDataThreadInfoQueue.Enqueue (new MapThreadInfo<MapData> (callback, mapData));
         }
     }
 
-    public void RequestMeshData (MapData mapData, Action<MeshData> callback) {
+    public void RequestMeshData (MapData mapData, int lod, Action<MeshData> callback) {
         ThreadStart threadStart = delegate {
-            MeshDataThread (mapData, callback);
+            MeshDataThread (mapData, lod, callback);
         };
 
         new Thread (threadStart).Start ();
     }
 
-    void MeshDataThread (MapData mapData, Action<MeshData> callback) {
-        MeshData meshData = MeshGenerator.GenerateTerrrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail);
+    void MeshDataThread (MapData mapData, int lod, Action<MeshData> callback) {
+        MeshData meshData = MeshGenerator.GenerateTerrrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
         lock (meshDataThreadInfoQueue) {
             meshDataThreadInfoQueue.Enqueue (new MapThreadInfo<MeshData> (callback, meshData));
         }
@@ -90,16 +92,17 @@ public class MapGenerator : MonoBehaviour {
         }
     }
 
-    MapData GenerateMapData () {
-        float[, ] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+    MapData GenerateMapData (Vector2 center) {
+        float[, ] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, center + offset, normalizeMode);
 
         Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
         for (int y = 0; y < mapChunkSize; y++) {
             for (int x = 0; x < mapChunkSize; x++) {
                 float currentHeight = noiseMap[x, y];
                 for (int i = 0; i < regions.Length; i++) {
-                    if (currentHeight <= regions[i].height) {
+                    if (currentHeight >= regions[i].height) {
                         colorMap[y * mapChunkSize + x] = regions[i].color;
+                    } else {
                         break;
                     }
                 }
